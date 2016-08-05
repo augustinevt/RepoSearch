e68fd2e7b5769faedceafb8d6250ba780b22f6ed
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-08-05 08:42:10 -0700

    add ability to search and retrieve users repositories

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -2,11 +2,11 @@
 var apiKey = '0574454a261f55acf62e0d6c53d12e38695ab617'
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    var user = $('#location').val();
-    $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + user + ".");
-    $.get('https://api.github.com/search/repositories?q=user:'+ user + '&access_token=' + apiKey).then(function(response) {
+  $('#username-button').click(function() {
+    var username = $('#username').val();
+    $('#username').val("");
+    $('.showWeather').text("The city you have chosen is " + username + ".");
+    $.get('https://api.github.com/search/repositories?q=user:'+ username + '&access_token=' + apiKey).then(function(response) {
        console.log(response);
      }).fail(function(error) {
        console.log(response);
